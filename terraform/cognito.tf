f8c6215601ba627e42b0995c77156d197284ca02
rafael.nakazono <rafael.nakazono@outlook.com.br>
2024-09-20 00:15:50 -0300

    code reorganization

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_version = "1.9.5"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.63.0"
-    }
-  }
-}
-
-data "aws_iam_role" "cognito_cluster_role" {
-  name = "LabRole"
-}
-
-provider "aws" {
-  region     = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  token      = var.AWS_SESSION_TOKEN
-}
-
 resource "aws_cognito_user_pool" "lanchonete_fiap_user_pool" {
   name = var.COGNITO_USER_POOL_NAME
 
